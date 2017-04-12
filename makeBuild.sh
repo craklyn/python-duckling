5d655648109a5eb9100099acb7e222fe2d749ab1
Daniel Blackburn <dblackburn@marchex.com>
2017-04-12 16:09:30 -0700

    Removed default jar and added script to get newest duckling jar from outside of repository

---
+++ makeBuild.sh
@@ -0,0 +1,9 @@
+cd ../duckling/
+lein deps
+lein uberjar
+cd ../python-duckling/
+cp /home/dblackburn/duckling/target/duckling-0.4.23.jar duckling/jars/.
+
+python setup.py build
+python setup.py install
+
